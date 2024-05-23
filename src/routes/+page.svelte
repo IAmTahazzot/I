43e2c109cb97f13f5ffb912359ff63b8fbc7ee2b
Tahazzot <97084738+IAmTahazzot@users.noreply.github.com>
2024-05-23 23:44:44 +0600

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,21 @@
+<div>
 <p>Under construction 🚧, I'll return on June 1, 2024.</p>
 
 <a href="mailto:iamtahazzot@gmail.com">For temporary inquiries, please click here.</a>
+</div>
+
+<style>
+div {
+ padding: 1em;
+}
+
+p,a{
+  font-family: monospace;
+}
+
+a {
+ color:#0077ff;
+padding: .5rem 0;
+display:block;
+}
+<style>
